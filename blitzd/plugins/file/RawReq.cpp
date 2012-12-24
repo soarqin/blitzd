ad7f74484f61c59a58ad3308883b91cdeabf47bb
Soar Qin <soarchin@verycd.com>
2012-12-24 16:35:58 +0800

    Added debug message

--- blitzd/plugins/file/RawReq.cpp
+++ blitzd/plugins/file/RawReq.cpp
@@ -15,8 +15,8 @@ namespace Plugins
 			st >> packet.fileName;
 			packet.adid = 0;
 			packet.extag = 0;
+			LOG_DEBUG(("Requesting file %s...", packet.fileName.c_str()));
 			packet.BuildAndSendTo(cl);
-
 			return true;
 		}
 	}
